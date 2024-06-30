9951c874b4fe9374001e69bba9a5066e7c6be7c6
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-06-30 23:23:14 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,19 @@
+# №2
+immutable_var = 'Mr. Green', 'and', 'Mr. Blue', False, 1941, 1945
+print(immutable_var)
+# №3
+# immutable_var[0] = 'Hulk'
+# print(immutable_var)
+# кортеж не поддерживает обращение по элементам,
+# кортежи в отличие от списков, являются не изменяемыми
+
+# №4
+mutable_list = (['book', 'notebook'], 'pen', 'diary')
+print(mutable_list)
+mutable_list[0][0] = 'pencil'
+print(mutable_list)
+
+
+
+
+
